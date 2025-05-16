b509bb724590315bd6178a3d022577b45aa6a68c
Jiří Bešta <jiribesta@protonmail.com>
2025-05-16 17:29:17 +0200

    modified, on god fr fr

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@
 
 print("Hello, world!")
 print("This is my program!")
+
+TOP_SECRET_NEVER_SHARE_WITH_ANYONE_NUCLEAR_CODES = ["toad", "banana"]
+# Ahh, it sure would be a shame if there was no way to erase history in git, good thing I'm so smart and handsome and that would never ever happen.
+
+print("Modification done!")
